45566e933349ff1e07d817f0f436b9cc962ab5de
shaunvan119 <shaunvan@live.com.au>
2022-04-07 20:18:03 +1000

    added reset function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// variables for the game state
+// Create variables for the game state
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
 
-// variables to store references to the necessary DOM nodes
+// Create variables to store references to the necessary DOM nodes
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
 const player1Scoreboard = document.getElementById("player1Scoreboard")
@@ -12,13 +12,19 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
-function displayButton() {
-    rollBtn.style.display = 'none'
-    resetBtn.style.display = 'block'
-
+function showDisplayButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
 }
 
- rollBtn.addEventListener("click", function() {
+function showRollButton() {
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+}
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", startGame) 
+ 
+ function startGame() {
     const randomNumber = Math.floor(Math.random() * 6) + 1
 
     if (player1Turn) {
@@ -35,19 +41,33 @@ function displayButton() {
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
         message.textContent = "Player 1 Turn"
-    }  
-    
-    if (player1Score >= 20) {
-        message.textContent = "Player 1 has won!"
-        displayButton()
-    } else if (player2Score >= 20) {
-        message.textContent = "Player 2 has won!"
-        displayButton()
     }
     
-    // 1. Check if a player has won. If so, change the message to "Player X has won!"
-    // 2. Hide the Roll Dice Button and show the Reset Button. Hint: use display none/block
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 has won! 🥳"
+        showDisplayButton()
+    } else if (player2Score >= 20) {
+        message.textContent = "Player 2 has won! 🎉"
+        showDisplayButton()
+    }
     
     player1Turn = !player1Turn
-})
- 
+}
+//
+resetBtn.addEventListener("click", reset)
+
+function reset() {
+   
+    message.textContent = "Player 1 Turn"
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    player1Score = 0
+    player2Score = 0
+    showRollButton()
+    player1Turn = true
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
