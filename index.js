e7722f84d88697c24554048b978f174ec435cce0
shaunvan119 <shaunvan@live.com.au>
2022-04-07 13:54:07 +1000

    updated if statements for reset button display

--- index.js
+++ index.js
@@ -12,29 +12,38 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
-rollBtn.addEventListener("click", function() {
-     const randomNumber = Math.floor(Math.random() * 6) + 1
-     console.log(randomNumber)
-     
-     // if statement to evaluate who turn is it
-     if (player1Turn) {
-         player1Score += randomNumber
-         player1Dice.textContent = randomNumber
-         player1Scoreboard.textContent = player1Score
-         message.textContent = "Player 1 rolled "
-         player1Dice.classList.remove("active");
-         player2Dice.classList.add("active");
-        
-     } else {
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
         player2Score += randomNumber
-        player2Dice.textContent = randomNumber 
         player2Scoreboard.textContent = player2Score
-        message.textContent = "Player 2 rolled "
-        player2Dice.classList.remove("active");
-         player1Dice.classList.add("active");
-       
-        
-     }
-     player1Turn = !player1Turn
- })
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }  
+    
+    if ( player1Score >= 20) {
+        message.textContent = "Player 1 has won!"
+        rollBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
+    } if (player2Score >= 20) {
+        message.textContent = "Player 2 has won!"
+        rollBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
+    }
+    
+    // 1. Check if a player has won. If so, change the message to "Player X has won!"
+    // 2. Hide the Roll Dice Button and show the Reset Button. Hint: use display none/block
+    
+    player1Turn = !player1Turn
+})
  
